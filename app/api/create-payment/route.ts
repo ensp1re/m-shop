e2e1d6a8f9926b49ceb26cbfb7d7180a161889c0
Ensp1re <116386083+ensp1re@users.noreply.github.com>
2024-01-19 22:28:13 +0200

    Update route.ts

--- app/api/create-payment/route.ts
+++ app/api/create-payment/route.ts
@@ -36,7 +36,7 @@ export async function POST(request: Request) {
 
         const body = await request.json()
         const { items, payment_intent_id } = body
-        const total = calculateOrderAmount(items) * 100
+        const total = Math.round(calculateOrderAmount(items) *100)
         const orderData = {
             user: { connect: { id: currentUser.id } },
             amount: total,
