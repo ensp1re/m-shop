d4d8247c4d4f38400a0a088ac9e500c064ee8405
ensp1re <ostapuk1318@gmail.com>
2024-10-11 21:57:45 +0300

    fixed and added some env

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,52 @@
 export const revalidate = 0;
 
-import Container from "@/app/components/container"
+import Container from "@/app/components/container";
 import HomeBanner from "@/app/components/HomeBanner/HomeBanner";
 import { products } from "@/utils/products";
-import { truncateText } from "@/utils/truncateText";
 import ProductCard from "@/app/components/products/ProductCard";
 import getProducts, { IProductParams } from "@/actions/getProducts";
 import { Product } from "@prisma/client";
 import NullData from "./components/NullData";
 
-
 interface HomeProps {
-    searchParams: IProductParams
+  searchParams: IProductParams;
 }
 
-export default async function Home({ searchParams } : HomeProps ) {
+export default async function Home({ searchParams }: HomeProps) {
+  const productsFromDatabase = await getProducts(searchParams);
 
-    const productsFromDatabase = await getProducts(searchParams)
+  if (productsFromDatabase.length === 0) {
+    return <NullData title="No products in database" />;
+  }
 
-    if (productsFromDatabase.length === 0) {
-        return <NullData title="No products in database" />
+  function shuffleArray(array: any) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i * 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
 
-    function shuffleArray(array: any) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i * 1));
-            [array[i], array[j]] = [array[j], array[i]]
-        }
+    return array;
+  }
 
-        return array
-    }
+  const allProducts: Product[] = [...products, ...productsFromDatabase];
 
+  const shuffledPropucts = shuffleArray(allProducts);
 
-    const allProducts: Product[] = [...products, ...productsFromDatabase]
-
-
-    const shuffledPropucts = shuffleArray(allProducts)
-
-
-    return (
-        <div className="p-8">
-            <Container>
-                <div>
-                    <HomeBanner />
-                </div>
-                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5
-                                2xl:grid-cols-6 gap-8">
-                    {shuffledPropucts.map((product: any) => {
-                        return <ProductCard data={product} key={product.id}/>
-                    })}
-                </div>
-            </Container>
+  return (
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
         </div>
-    )
+        <div
+          className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5
+                                2xl:grid-cols-6 gap-8"
+        >
+          {shuffledPropucts.map((product: any) => {
+            return <ProductCard data={product} key={product.id} />;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
 }
--- providers/CartProvider.tsx
+++ providers/CartProvider.tsx
@@ -1,16 +1,13 @@
-'use client'
+"use client";
 
-import {CartContextProvider} from "@/hooks/useCart";
+import { CartContextProvider } from "@/hooks/useCart";
 
 interface CartProviderProps {
-    children : React.ReactNode
+  children: React.ReactNode;
 }
 
-
-const CartProvider: React.FC<CartProviderProps> = ({children}) =>{
-    return (
-        <CartContextProvider>{children}</CartContextProvider>
-    )
-}
+const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  return <CartContextProvider>{children}</CartContextProvider>;
+};
 
 export default CartProvider;
